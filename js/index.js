8e80653b7fe0fc37858735c19730d758913aa5c8
Antoine <antoineg.work@gmail.com>
2019-11-15 05:08:12 +0100

    Finalisation

--- js/index.js
+++ js/index.js
@@ -14,6 +14,39 @@ function toogleMenu() {
             toToogle.item(i).style.display = "none";
         }
     }
+
+    
+    let toogleLogo = document.getElementById("toogle");
+    console.log("ToogleLogo : " + toogledMenu);
+    if(toogledMenu) {
+        toogleLogo.setAttribute("src", "images/crossBlack.png");
+    } else {
+        toogleLogo.setAttribute("src", "images/toogle.png");
+    }
+
+    toogledMenu = !toogledMenu;
+}
+function toogleMenu2() {
+
+    let toToogle = document.getElementsByClassName("item");
+
+    for(let i = 0 ; i < toToogle.length ; i++) {
+        if(toogledMenu) {
+            toToogle.item(i).style.display = "block";
+        } else {
+            toToogle.item(i).style.display = "none";
+        }
+    }
+
+    
+    let toogleLogo = document.getElementById("toogle");
+    console.log("ToogleLogo : " + toogledMenu);
+    if(toogledMenu) {
+        toogleLogo.setAttribute("src", "../images/crossBlack.png");
+    } else {
+        toogleLogo.setAttribute("src", "../images/toogle.png");
+    }
+
     toogledMenu = !toogledMenu;
 }
 
